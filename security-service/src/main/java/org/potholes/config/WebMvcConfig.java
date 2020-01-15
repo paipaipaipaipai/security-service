7ffc3a2dd20c91d6f5af46797e3d8cfe425f92fa
paipaipaipaipai <243677721@qq.com>
2020-01-15 18:26:10 +0800

    123

--- security-service/src/main/java/org/potholes/config/WebMvcConfig.java
+++ security-service/src/main/java/org/potholes/config/WebMvcConfig.java
@@ -1,6 +1,9 @@
 package org.potholes.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,6 +14,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowedHeaders("*").allowCredentials(true)
                 .maxAge(3600);
+    }
+
+    @Bean
+    public CookieSerializer httpSessionIdResolver() {
+        DefaultCookieSerializer cookieSerializer = new DefaultCookieSerializer();
+        cookieSerializer.setSameSite(null);
+        return cookieSerializer;
     }
 
 }
