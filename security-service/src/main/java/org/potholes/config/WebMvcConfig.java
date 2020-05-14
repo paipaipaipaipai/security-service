4e5a47ecbca15323d6445792c3284d7502ece206
dylan_xu@amaxgs.local <dylan_xu@amaxgs.local>
2020-05-14 09:00:11 +0800

    1

--- security-service/src/main/java/org/potholes/config/WebMvcConfig.java
+++ security-service/src/main/java/org/potholes/config/WebMvcConfig.java
@@ -13,12 +13,15 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowedHeaders("*").allowCredentials(true)
-                .maxAge(3600);
+            .maxAge(3600);
     }
 
     @Bean
     public CookieSerializer httpSessionIdResolver() {
         DefaultCookieSerializer cookieSerializer = new DefaultCookieSerializer();
+        // session共享/跨域
+        // 前端请求到后台，每次session都不一样，每次都是新的会话，导致获取不到用户信息
+        // 取消仅限同一站点设置
         cookieSerializer.setSameSite(null);
         return cookieSerializer;
     }
